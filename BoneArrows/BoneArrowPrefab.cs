c01563b4d0268a228e530c2198ff0f8d94ea8c34
v801 <32247825+v801@users.noreply.github.com>
2021-03-30 03:28:28 -0700

    init commit

---
+++ BoneArrows/BoneArrowPrefab.cs
@@ -0,0 +1,25 @@
+﻿using JotunnLib.Entities;
+
+namespace BoneArrows
+{
+    public class BoneArrowPrefab : PrefabConfig
+    {
+        public BoneArrowPrefab() : base("ArrowBone", "ArrowFlint")
+        {
+
+        }
+
+        public override void Register()
+        {
+            ItemDrop item = Prefab.GetComponent<ItemDrop>();
+            item.m_itemData.m_shared.m_itemType = ItemDrop.ItemData.ItemType.Ammo;
+            item.m_itemData.m_shared.m_name = "Bone Arrow";
+            item.m_itemData.m_shared.m_description = "Jagged bone tears the flesh.";
+            item.m_itemData.m_dropPrefab = Prefab;
+            item.m_itemData.m_shared.m_weight = 0.1f;
+            item.m_itemData.m_shared.m_maxStackSize = 100;
+            item.m_itemData.m_shared.m_variants = 1;
+            item.m_itemData.m_shared.m_damages.m_pierce = 30;
+        }
+    }
+}
---
+++ BoneArrows/BoneArrows.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using BepInEx;
+using JotunnLib.Managers;
+using JotunnLib.Entities;
+
+namespace BoneArrows
+{
+    [BepInPlugin("com.bepinex.plugins.bonearrows", "BoneArrows", "0.1.0")]
+    [BepInDependency("com.bepinex.plugins.jotunnlib")]
+    public class BoneArrows : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            ObjectManager.Instance.ObjectRegister += registerObjects;
+            PrefabManager.Instance.PrefabRegister += registerPrefabs;
+        }
+
+        private void registerPrefabs(object sender, EventArgs e)
+        {
+            PrefabManager.Instance.RegisterPrefab(new BoneArrowPrefab());
+        }
+
+        private void registerObjects(object sender, EventArgs e)
+        {
+            ObjectManager.Instance.RegisterItem("ArrowBone");
+
+            ObjectManager.Instance.RegisterRecipe(new RecipeConfig()
+            {
+                Name = "Recipe_BoneArrows",
+                Item = "ArrowBone",
+                Amount = 20,
+                CraftingStation = "piece_workbench",
+
+                Requirements = new PieceRequirementConfig[]
+                {
+                    new PieceRequirementConfig()
+                    {
+                        Item = "Wood",
+                        Amount = 8
+                    },
+                    new PieceRequirementConfig()
+                    {
+                        Item = "Feathers",
+                        Amount = 2
+                    },
+                    new PieceRequirementConfig()
+                    {
+                        Item = "BoneFragments",
+                        Amount = 5
+                    },
+                }
+            });
+        }
+    }
+}
